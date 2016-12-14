6ceb9f1e0cf8fd5a7e5e64b802168cd8bcbdeb39
binking <jiangzhibin2014.xujie@gmail.com>
2016-12-14 16:42:26 +0800

    let it go

--- scraping_weibo_followers_v2.py
+++ scraping_weibo_followers_v2.py
@@ -55,7 +55,7 @@ def user_info_generator(cache1, cache2):
             account = pick_rand_ele_from_list(all_account)
             # operate spider
             spider = WeiboFollowSpider(job, account, WEIBO_ACCOUNT_PASSWD, timeout=20)
-            spider.use_abuyun_proxy()
+            # spider.use_abuyun_proxy()
             spider.add_request_header()
             # spider.use_cookie_from_curl(WEIBO_MANUAL_COOKIES[account])
             spider.use_cookie_from_curl(TEST_CURL_SER)
@@ -133,7 +133,7 @@ def run_all_worker():
     result_pool = mp.Pool(processes=8, 
         initializer=user_db_writer, initargs=(result_cache, ))
     create_processes(add_jobs, (job_cache, ), 1)
-    
+
     cp = mp.current_process()
     print dt.now().strftime("%Y-%m-%d %H:%M:%S"), "Run All Works Process pid is %d" % (cp.pid)
     try:
