1e73a75f82d7d765340ef502909f7c77995687e6
binking <jiangzhibin2014.xujie@gmail.com>
2017-02-22 11:55:02 +0800

    debug

---
+++ fabfile.py
@@ -0,0 +1,16 @@
+#coding=utf8
+from fabric.api import *
+from zc_spider.weibo_config import SERVER_LIST, SERVER_LOGIN_INFO
+
+env.hosts=
+
+env.passwords = 
+
+def update():
+    with cd('/PythonProject/WeiboFollowersRelation'):
+        run('git pull')
+
+def execute():
+    with cd('/PythonProject/WeiboFollowersRelation'):
+        run('/home/jiangzhibin/PythonProgramming/ZhicangSpiders/spider_env/bin/python scraping_weibo_followers_v2.py >> ~/spider_log/weibo_follow.log 2>&1 &')
+
